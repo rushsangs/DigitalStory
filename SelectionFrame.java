d6475783866c92f74ada310d07c30c0a849bebc7
dh624 <dh624@scarletmail.rutgers.edu>
2016-04-03 23:45:32 -0400

    selection frame done

--- SelectionFrame.java
+++ SelectionFrame.java
@@ -5,12 +5,21 @@ import java.util.List;
 import java.awt.*;
 import java.awt.event.*;
 public class SelectionFrame extends JFrame implements ActionListener{
+	String[] list = null;
+	boolean[] bool = null;
+	String prompt = null;
+	private JLabel[] mylabels;
+	private JCheckBox[] myboxes;
+	private JButton ok = new JButton("OK");
 	
 	public SelectionFrame(String[] list, boolean[] bool, String prompt){
+		this.list = list;
+		this.bool = bool;
+		this.prompt = prompt;
 		this.setLayout(new BorderLayout(10,10));
 		this.setTitle(prompt);
-		JLabel[] mylabels = new JLabel[list.length];
-		JCheckBox[] myboxes = new JCheckBox[list.length];
+		mylabels = new JLabel[list.length];
+		myboxes = new JCheckBox[list.length];
 		JPanel mygrid = new JPanel();
 		mygrid.setLayout(new GridLayout(list.length,2));
 		for(int i = 0; i < list.length; i++){
@@ -19,6 +28,8 @@ public class SelectionFrame extends JFrame implements ActionListener{
 			myboxes[i].setSelected(true);
 			myboxes[i].setVisible(true);
 		}
+		this.add(mygrid,BorderLayout.CENTER);
+		this.add(ok,BorderLayout.SOUTH);
 		
 		
 		
@@ -29,6 +40,16 @@ public class SelectionFrame extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		switch(e.getActionCommand()){
+		case "OK":
+			for(int i = 0; i < myboxes.length; i++){
+				bool[i] = myboxes[i].isSelected();
+			}
 		
+		}
+	}
+	public boolean[] getBoolean(){
+		return bool;
+	
 	}
 }
