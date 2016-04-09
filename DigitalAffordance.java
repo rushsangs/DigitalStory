3355868289c3737f88f94795a31f637e10a98cef
James Kersh <james.kersh@rutgers.edu>
2016-04-08 20:27:29 -0400

    Fixed Affordance

--- DigitalAffordance.java
+++ DigitalAffordance.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 
 class ActionTuple {
 	public DigitalObject affordee;
-	public DigitalState active;
+	public boolean active;
 	public ActionType type;
 	
-	public ActionTuple(DigitalObject affordee, DigitalState active, ActionType type) {
+	public ActionTuple(DigitalObject affordee, boolean active, ActionType type) {
 		this.affordee = affordee;
 		this.active = active;
 		this.type = type;
@@ -17,10 +17,34 @@ public class DigitalAffordance {
 	//add state here
 	public String name;
 	public ArrayList<ActionTuple> instances;
-	public DigitalAffordance(String name, ArrayList<ActionTuples> instances) {
+	public DigitalAffordance(String name, ArrayList<ActionTuple> instances) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.instances = instances;
+	}
+	
+	public void addActionTuple(DigitalObject affordee, ActionType type) {
+		if(!contains(affordee, type)) {
+			instances.add(new ActionTuple(affordee, false, type));
+		}
+	}
+	
+	public void addActionTuple(ArrayList<DigitalObject> affordees, ActionType type) {
+		for(int i=0; i < affordees.size(); i++) {
+			addActionTuple(affordees.get(i), type);
+		}
+	}
+	
+	public boolean contains(DigitalObject affordee, ActionType type) {
+		for(int i = 0; i < instances.size(); i++) {
+			ActionTuple a = instances.get(i);
+			if(a.affordee.equals(affordee) && a.type == type) {
+				//duplicate action
+				//side effect for repeated actions
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
