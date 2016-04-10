0b51094ff047aa1ca904077b2a11dc6679d9b27f
James Kersh <james.kersh@rutgers.edu>
2016-04-09 21:03:59 -0400

    Updated Story Generator RK9 algo to work with new structure

--- StoryGenerator.java
+++ StoryGenerator.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class StoryGenerator {
 	
 	enum StoryAlgorithm { 
-		STARTFALSE, STARTRANDOM
+		RK9, KIPPS
 	}
 	
 	private ArrayList<DigitalObject> objects;
@@ -14,7 +14,7 @@ public class StoryGenerator {
 	
 	public StoryGenerator(ArrayList<DigitalObject> objs) {
 		objects = objs;
-		setStoryAlgorithm(StoryAlgorithm.STARTFALSE); //default story algo is start false
+		setStoryAlgorithm(StoryAlgorithm.KIPPS); //default story algo is start false
 	}
 	
 	public StoryGenerator(ArrayList<DigitalObject> objs, StoryAlgorithm e) {
@@ -29,50 +29,47 @@ public class StoryGenerator {
 		String result = "";
 		//set up initial state of the world
 		switch(sa) {
-		case STARTFALSE:
+		case RK9:
 			for(int i = 0; i < objects.size(); i++) {
-				for(int j = 0; j < objects.get(i).states.size(); j++) {
-					objects.get(i).states.get(j).isActive = false;
-				}
-			}
-			break;
-		case STARTRANDOM:
-			for(int i = 0; i < objects.size(); i++) {
-				for(int j = 0; j < objects.get(i).states.size(); j++) {
-					objects.get(i).states.get(j).isActive = rng.nextBoolean();
-				}
-			}
-			break;
-		}
-		//for each line of the story
-		System.out.println("line 48");
-		for(int i = 0; i < length; i++) {
-			//pick an active object to perform an action 
-			boolean active = false;
-			int obj = 0;
-			while (!active) {
-				obj = rng.nextInt(objects.size());
-				if(objects.get(obj).actions.size() != 0) {
-					active = true;
-				}
-			}
-			result += objects.get(obj).name + " ";
-			//pick an action for it to perform
-			int act = rng.nextInt(objects.get(obj).actions.size());
-			result += objects.get(obj).actions.get(act).affordance.name + " ";
-			//pick at least one object for it to happen to
-			boolean[] affs = new boolean[objects.get(obj).actions.get(act).affordees.size()];
-			boolean min = false;
-			while(!min) {
-				for(int j = 0; j < affs.length; j++) {
-					affs[j] = rng.nextBoolean();
-					if(affs[j]) {
-						min = true;
-						result += objects.get(obj).actions.get(act).affordees.get(j).name + " ";
+				for(int j = 0; j < objects.get(i).affordances.size(); j++) {
+					for(int k = 0; k < objects.get(i).affordances.get(j).instances.size(); k++) {
+						objects.get(i).affordances.get(j).instances.get(k).active = false;
 					}
 				}
 			}
-			result += "\n";
+			//for each line of the story
+			for(int i = 0; i < length; i++) {
+				//pick an active object to perform an action 
+				boolean active = false;
+				int obj = 0;
+				while (!active) {
+					obj = rng.nextInt(objects.size());
+					if(!objects.get(obj).isPassive) {
+						active = true;
+					}
+				}
+				result += objects.get(obj).name + " ";
+				//pick an action for it to perform
+				int act = rng.nextInt(objects.get(obj).affordances.size());
+				result += objects.get(obj).affordances.get(act).name + " ";
+				//pick at least one object for it to happen to
+				boolean[] affs = new boolean[objects.get(obj).affordances.get(act).instances.size()];
+				boolean min = false;
+				while(!min) {
+					for(int j = 0; j < affs.length; j++) {
+						affs[j] = rng.nextBoolean();
+						if(affs[j]) {
+							min = true;
+							result += objects.get(obj).affordances.get(act).instances.get(j).affordee.name + " ";
+						}
+					}
+				}
+				result += "\n";
+			}
+			break;
+		case KIPPS:
+			
+			break;
 		}
 		return result;
 	}
@@ -80,16 +77,16 @@ public class StoryGenerator {
 	public void setStoryAlgorithm(StoryAlgorithm e) {
 		sa = e;
 		switch(e) {
-			case STARTFALSE:
+			case RK9:
 				MAX_LENGTH = 10;
 				MIN_LENGTH = 5;
 				break;
-			case STARTRANDOM:
+			case KIPPS:
 				MAX_LENGTH = 40;
 				MIN_LENGTH = 10;
 				break;
 			default:
-				setStoryAlgorithm(StoryAlgorithm.STARTFALSE);
+				setStoryAlgorithm(StoryAlgorithm.KIPPS);
 				break;
 		}
 	}
