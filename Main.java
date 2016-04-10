997db1421c95dc67a666115b13ab26fee3626620
aznkid66 <aznkid66@gmail.com>
2016-04-10 12:33:27 -0400

    commented out deprecated, unsupported methods

--- Main.java
+++ Main.java
@@ -21,57 +21,57 @@ public class Main {
 //		String story=br.readLine();
 //		printData(analyze(story, objects));
 	}
-	
-	private static ArrayList<DigitalObject> analyze(String story, ArrayList<DigitalObject> objects)
-	{
-		//read the story sentence by sentence, separated by period
-		String[] sentences = story.split("\\.");
-		
-
-		
-		//for each sentence: first word is Active Object, second word is action, third word is passive object?
-		for(int i =0;i<sentences.length; ++i)
-		{
-			sentences[i]=sentences[i].trim();
-			String[] words=sentences[i].split(" ");
-			//first word is active object
-			DigitalObject object = null;
-			boolean found=false;
-			for(int j=0; j<objects.size(); ++j)
-			{
-				if(objects.get(j).name.equalsIgnoreCase(words[0]))
-				{
-					object=objects.get(j);
-					found=true;
-					break;
-				}
-			}
-			if(!found)
-			{
-				object = new DigitalObject(words[0]);
-				objects.add(object);
-				
-			}
-			
-			//second word is action, add to active object
-			object.addAction(new DigitalAction(new DigitalAffordance(words[1]), new ArrayList<DigitalObject>(), new DigitalState(words[1].concat(words[2]))));
-			//TODO: third word: what do we do with it?
-			
-		}
-		return objects;
-	}
-	public static void printData(ArrayList<DigitalObject> objects)
-	{
-		for(int i =0; i < objects.size(); ++i)
-		{
-			DigitalObject object=objects.get(i);
-			System.out.println("Object is :"+object.name);
-			ArrayList<DigitalAction> actions= object.actions;
-			for( int j = 0; j<actions.size();++j)
-			{
-				System.out.println("Action for this object is: "+actions.get(j).affordance.name);
-			}
-		}
-	}
+//	
+//	private static ArrayList<DigitalObject> analyze(String story, ArrayList<DigitalObject> objects)
+//	{
+//		//read the story sentence by sentence, separated by period
+//		String[] sentences = story.split("\\.");
+//		
+//
+//		
+//		//for each sentence: first word is Active Object, second word is action, third word is passive object?
+//		for(int i =0;i<sentences.length; ++i)
+//		{
+//			sentences[i]=sentences[i].trim();
+//			String[] words=sentences[i].split(" ");
+//			//first word is active object
+//			DigitalObject object = null;
+//			boolean found=false;
+//			for(int j=0; j<objects.size(); ++j)
+//			{
+//				if(objects.get(j).name.equalsIgnoreCase(words[0]))
+//				{
+//					object=objects.get(j);
+//					found=true;
+//					break;
+//				}
+//			}
+//			if(!found)
+//			{
+//				object = new DigitalObject(words[0]);
+//				objects.add(object);
+//				
+//			}
+//			
+//			//second word is action, add to active object
+//			object.addAction(new DigitalAction(new DigitalAffordance(words[1]), new ArrayList<DigitalObject>(), new DigitalState(words[1].concat(words[2]))));
+//			//TODO: third word: what do we do with it?
+//			
+//		}
+//		return objects;
+//	}
+//	public static void printData(ArrayList<DigitalObject> objects)
+//	{
+//		for(int i =0; i < objects.size(); ++i)
+//		{
+//			DigitalObject object=objects.get(i);
+//			System.out.println("Object is :"+object.name);
+//			ArrayList<DigitalAction> actions= object.actions;
+//			for( int j = 0; j<actions.size();++j)
+//			{
+//				System.out.println("Action for this object is: "+actions.get(j).affordance.name);
+//			}
+//		}
+//	}
 
 }
