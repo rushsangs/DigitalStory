106706d15782800b5fc3df77dbfd08dceb780e5a
aznkid66 <aznkid66@gmail.com>
2016-05-11 22:47:36 -0400

    precondition question update

--- PreconditionQuestion.java
+++ PreconditionQuestion.java
@@ -5,6 +5,8 @@ public class PreconditionQuestion implements Question {
 	String b;
 	String m;
 	String e;
+	boolean[][] matrix1;
+	boolean[][] matrix2;
 	
 	public PreconditionQuestion(DigitalStoryWorld world, String b, String m, String e) {
 		this.world = world;
@@ -29,27 +31,29 @@ public class PreconditionQuestion implements Question {
 			meA[meA_index] = eA[i];
 		}
 		
-		boolean[][] matrix1 = new boolean[bA.length][meA.length];
-		boolean[][] matrix2 = new boolean[mA.length][eA.length];
+		matrix1 = new boolean[bA.length][meA.length];
+		matrix2 = new boolean[mA.length][eA.length];
 		
 		PreconditionFrame frame1 = new PreconditionFrame("alskjfs #1", bA, meA, matrix1);
 		frame1.setLocationRelativeTo(null);
 		frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame1.pack();
 		frame1.setVisible(true);
+		// matrix1 populated
 		
-		PreconditionFrame frame2 = new PreconditionFrame("alskjfs #1", bA, meA, matrix2);
+		PreconditionFrame frame2 = new PreconditionFrame("alskjfs #2", bA, meA, matrix2);
 		frame2.setLocationRelativeTo(null);
 		frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame2.pack();
 		frame2.setVisible(true);
+		// matrix2 populated
 
 	}
 
 	@Override
 	public void applyAnswer() {
 		// TODO Auto-generated method stub
-
+		
 	}
 	
 	private String sqlStatement(String name1, String name2) {
