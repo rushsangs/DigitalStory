c3834ba1e8b0a1c2cfbe2964ddb37c8dddcba05f
aznkid66 <aznkid66@gmail.com>
2016-09-29 03:46:27 -0400

    configured tester for kapadia meeting

--- PrologTester.java
+++ PrologTester.java
@@ -1,21 +1,11 @@
-
-
-import java.nio.file.Files;
+import java.util.Scanner;
 
 public class PrologTester {
 	public static void main(String[] args) {
-		PrologQueryMaster pqm = new PrologQueryMaster("patrick_test.pro");
-		if (pqm.query("action", new String[]{"ned", "stop", "her"})) {
-			System.out.println("ned stop her true");
-		}
-		if (pqm.query("action", new String[]{"X", "stop", "her"})) {
-			System.out.println("X stop her true");
-		}
-		if (pqm.query("action", new String[]{"john", "stop", "her"})) {
-			System.out.println("john stop her true");
-		}
-		if (pqm.query("action", new String[]{"stark", "met", "robert"})) {
-			System.out.println("stark met robert true");
+		PrologQueryMaster pqm = new PrologQueryMaster("prolog/facts.pro");
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			System.out.println(pqm.query(sc.nextLine(), sc.nextLine().split(" ")));
 		}
 		
 	}
