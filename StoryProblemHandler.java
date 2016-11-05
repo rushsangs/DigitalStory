c214439ab43a524321c245087fb888f062688291
aznkid66 <aznkid66@gmail.com>
2016-11-05 02:14:46 -0400

    made sure storyproblemhandler didn't have concurrent list editing errors

--- StoryProblemHandler.java
+++ StoryProblemHandler.java
@@ -1,28 +1,30 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 
 public class StoryProblemHandler implements Runnable {
-	public ArrayList<StoryProblemObject[]> problemsList;
+	public int lastIndex;
+	public LinkedHashMap<Integer, String[]> updatedSentencesList;
+	
 	public HashMap<Integer, String[]> sentences;
-	public LinkedHashSet<Integer> updatedSentencesList;
+	
+	public ArrayList<StoryProblemObject[]> problemsList;
 	
 	public StoryProblemHandler() {
+		lastIndex = -1;
+		updatedSentencesList = new LinkedHashMap<Integer, String[]>();
+		
 		problemsList = new ArrayList<StoryProblemObject[]>();
 		sentences = new HashMap<Integer, String[]>();
-		updatedSentencesList = new LinkedHashSet<Integer>();
 	}
 	
 	public void appendSentence (String oaoText, String nLText) {
-		problemsList.add(null);
-		sentences.put(problemsList.size()-1, new String[]{oaoText, nLText});
-		updatedSentencesList.add(problemsList.size()-1);
+		lastIndex++;
+		updatedSentencesList.put(lastIndex, new String[]{oaoText, nLText});
 	}
 	
 	public void updateSentence(int index, String oaoText, String nLText) {
-		problemsList.set(index, null);
-		sentences.put(index, new String[]{oaoText, nLText});
-		updatedSentencesList.add(index);
+		updatedSentencesList.put(index, new String[]{oaoText, nLText});
 	}
 	
 	public boolean detectProblems() {
@@ -43,9 +45,11 @@ public class StoryProblemHandler implements Runnable {
 	public void run() {
 		while (true) {
 			if (!updatedSentencesList.isEmpty()) {
-				int nextIndex = updatedSentencesList.iterator().next();
-				detectProblems(nextIndex);
+				int nextIndex = updatedSentencesList.keySet().iterator().next();
+				sentences.put(nextIndex, updatedSentencesList.get(nextIndex));
 				updatedSentencesList.remove(nextIndex);
+				
+				detectProblems(nextIndex);
 			}
 		}
 	}
